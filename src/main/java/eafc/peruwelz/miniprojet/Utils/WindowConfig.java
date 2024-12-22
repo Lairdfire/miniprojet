39d70f8a143f26a8b464795274577ce13858b996
Arnaud Parmentier <arnaud.parmentier@proton.me>
2024-12-22 22:45:22 +0100

    Base project

---
+++ src/main/java/eafc/peruwelz/miniprojet/Utils/WindowConfig.java
@@ -0,0 +1,16 @@
+package eafc.peruwelz.miniprojet.Utils;
+
+public class WindowConfig {
+
+    // Chemins des fichiers FXML
+    public static final String ALBUMS_VIEW = "/views/AlbumsView.fxml";
+    public static final String PLAYLISTS_VIEW = "/views/PlaylistView.fxml";
+    public static final String ADD_SONG_VIEW = "/views/AddSongView.fxml";
+    public static final String CATALOG_VIEW = "/views/CatalogView.fxml";
+
+    // Titres des fenêtres
+    public static final String ALBUMS_TITLE = "Albums";
+    public static final String PLAYLISTS_TITLE = "Playlists";
+    public static final String ADD_SONG_TITLE = "Add a song";
+    public static final String CATALOG_TITLE = "Catalog";
+}
